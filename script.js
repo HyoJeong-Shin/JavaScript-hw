4435530c9b94578fc6f16451d8997c913765197a
HyoJeongShin <kyn05298@naver.com>
2020-08-18 13:08:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let t1 = document.getElementById('naver');
+let t2 = document.getElementById('daum');
+let t3 = document.getElementById('duksung');
+
+t1.addEventListener('click', function(event){
+  window.open('https://www.naver.com/');
+});
+
+t2.addEventListener('click', function(event){
+  window.open('https://www.daum.net/');
+});
+
+t3.addEventListener('click', function(event){
+  window.open('https://www.duksung.ac.kr/');
+});
